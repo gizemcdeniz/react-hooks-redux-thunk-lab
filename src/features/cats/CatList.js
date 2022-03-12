251213e296b0432f02cd3cba22428f1444538d96
gizem <gizemc.deniz@gmail.com>
2022-03-12 19:32:36 +0300

    done

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,6 @@
-// write your CatList component here
+export default function CatList({catPics=[]}) {
+
+    return catPics.map((item,i)=>(
+        <img src={item.url} alt="cat" key ={i}/>
+    ))
+}
--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,10 +1,17 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import CatList from "./CatList"
+import {useSelector,useDispatch}from "react-redux"
+import { fetchCats } from "./catsSlice";
 function Cats() {
+  const dispatch =useDispatch()
+  const catPics=useSelector(state=>state.entities)
+   useEffect(()=>{
+    dispatch(fetchCats())
+      },[dispatch])
   return (
     <div>
       <h1>CatBook</h1>
-      {/* add CatList component here */}
+      <CatList catPics={catPics} />
     </div>
   );
 }
